1af08ef27ec0a4669dff9aeabbed56917f1e016d
Ricardo Vargas <r.sagrero1@gmail.com>
2019-08-13 17:02:53 -0500

    Ejercio 6

---
+++ repaso_apuntadores_ver1.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <string.h>
+int main(){
+    char caracter = 0; 
+    int entero = 0;
+    float flotante = 0; 
+    long largo = 0; 
+    double doble = 0; 
+    printf(" char =   %p %ld\n",&caracter,sizeof(caracter));
+    printf(" int =    %p %ld\n",&entero,sizeof(int));
+    printf(" float =  %p %ld\n",&flotante,sizeof(float));
+    printf(" long =   %p %ld\n",&largo,sizeof(long));
+    printf(" double = %p %ld\n",&doble,sizeof(double));
+    char cadena [] = "ESCOM - IPN";
+    int i = 0, length = strlen(cadena); 
+    for(i = 0; i < length+1; i++){
+        printf("cadena [%d] %c %.2X = %p\n",i,cadena[i],cadena[i],&cadena[i]);
+    }
+    printf("\n%c\n",cadena+4);
+}
