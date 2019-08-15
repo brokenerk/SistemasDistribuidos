5419c561ff5c671c8fdf03be2f975e6ee1c9231e
erictoman <ericto117@gmail.com>
2019-08-15 11:41:00 -0500

    Repaired

---
+++ PrincipiosC++/E4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double c = 20;
+    double f;
+    f = (9 / 5) * c + (double)32;
+cout << f << endl;
+}
---
+++ PrincipiosC++/E5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 2;
+    n + (++n);
+    cout << n << endl;
+}
