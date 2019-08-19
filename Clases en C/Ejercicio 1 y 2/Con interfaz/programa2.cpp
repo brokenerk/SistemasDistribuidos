06068f1e32f78e634ca4c335deb67bfb8a310946
erictoman <ericto117@gmail.com>
2019-08-19 11:17:29 -0500

    E1 & E2 done

--- Clases en C/Ejercicio 1 y 2/Con interfaz/programa2.cpp
+++ Clases en C/Ejercicio 1 y 2/Con interfaz/programa2.cpp
@@ -1,21 +1,12 @@
 #include "Fecha.h"
 #include <iostream>
 using namespace std;
-Fecha::Fecha(int dd, int mm, int aaaa)
+int main()
 {
- mes = mm;
- dia = dd;
- anio = aaaa;
-}
-void Fecha::inicializaFecha(int dd, int mm, int aaaa)
-{
- anio = aaaa;
- mes = mm;
- dia = dd;
- return;
-}
-void Fecha::muestraFecha()
-{
- cout << "La fecha es(dia-mes-año): " << dia << "-" << mes << "-" << anio << endl;
- return;
+    Fecha a, b, c(21, 9, 1973);
+
+    b.inicializaFecha(17, 6, 2000);
+    a.muestraFecha();
+    b.muestraFecha();
+    c.muestraFecha();
 }
