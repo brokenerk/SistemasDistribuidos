516de932f616867fe48cd04cf30ad18fc521b2eb
brokenerk <enriquebroly@hotmail.com>
2019-08-20 09:25:42 -0500

    ejercicio 2 constructores

---
+++ Const-Dest/E2.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h> 
+using namespace std;
+
+class Fecha
+{
+	private:
+
+	public:
+		int dia;
+	    int mes;
+	    int anio;
+	    Fecha(int = 3, int = 4, int = 2014);
+	    void inicializaFecha(int, int, int);
+	    void muestraFecha();
+};
+
+Fecha::Fecha(int dd, int mm, int aaaa)
+{
+    mes = mm;
+    dia = dd;
+    anio = aaaa;
+}
+
+void Fecha::inicializaFecha(int dd, int mm, int aaaa)
+{
+    anio = aaaa;
+    mes = mm;
+    dia = dd;
+    return;
+}
+
+void Fecha::muestraFecha()
+{
+    cout << "La fecha es(dia-mes-año): " << dia << "-" << mes << "-" << anio << endl;
+    return;
+}
+
+int masVieja(Fecha fecha1, Fecha fecha2)
+{
+	if(fecha1.anio > fecha2.anio){
+		return 1;
+	}
+	else if(fecha1.anio == fecha2.anio)
+	{
+		if(fecha1.mes > fecha2.mes)
+		{
+			return 1;
+		}
+		else if(fecha1.mes == fecha2.mes)
+		{
+			if(fecha1.dia > fecha2.dia)
+			{
+				return 1;
+			}
+			else if(fecha1.dia == fecha2.dia){
+				return 0;
+			}
+			else{
+				return -1;
+			}
+		}
+		else{
+			return -1;
+		}
+	}
+	else{
+		return -1;
+	}
+		
+}
+
+int masVieja1(Fecha &fecha1, Fecha &fecha2)
+{
+	if(fecha1.anio > fecha2.anio){
+		return 1;
+	}
+	else if(fecha1.anio == fecha2.anio)
+	{
+		if(fecha1.mes > fecha2.mes)
+		{
+			return 1;
+		}
+		else if(fecha1.mes == fecha2.mes)
+		{
+			if(fecha1.dia > fecha2.dia)
+			{
+				return 1;
+			}
+			else if(fecha1.dia == fecha2.dia){
+				return 0;
+			}
+			else{
+				return -1;
+			}
+		}
+		else{
+			return -1;
+		}
+	}
+	else{
+		return -1;
+	}
+		
+}
+
+int main()
+{
+	srand (time(NULL));
+    for(int i = 0; i <10000; i++){
+    	int anio1 = rand() % 40000000 + 1;
+    	int mes1 = rand() % 12 + 1;
+    	int dia1 = rand() % 30 + 1;
+
+    	int anio2 = rand() % 40000000 + 1; 
+    	int mes2 = rand() % 12 + 1;
+    	int dia2 = rand() % 30 + 1;
+
+
+    	Fecha a(dia1, mes1, anio1), b(dia2, mes2, anio2); 
+    	//masVieja(a, b);
+    	masVieja1(a, b);
+    }
+    
+}
