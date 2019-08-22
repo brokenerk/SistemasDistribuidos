16371aa13b4752dfd47918a68871dfaa0fb7e754
Marcus <rafassassinscreed@gmail.com>
2019-08-22 11:17:05 -0500

    lo del main

--- Composicion/E1 (copia)/prac_compo.cpp
+++ Composicion/E1 (copia)/prac_compo.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int main() {
-	Rectangulo rectangulo1(2,3,5,1);
+	Rectangulo rectangulo1( Coordenada(2,3), Coordenada(5,1));
+
 	double ancho, alto;
 	cout << "Calculando el área de un rectángulo dadas sus coordenadas en un plano cartesiano:\n";
 	rectangulo1.imprimeEsq();
 	alto = rectangulo1.obtieneSupIzq().obtenerY() -rectangulo1.obtieneInfDer().obtenerY();
 	ancho = rectangulo1.obtieneInfDer().obtenerX() -rectangulo1.obtieneSupIzq().obtenerX();
-	cout << "El área del rectángulo es = " << ancho*alto << endl;
+	cout << "El área del rectángulo es = " << rectangulo1.obtieneArea(alto,ancho) << endl;
 	return 0;
 }
