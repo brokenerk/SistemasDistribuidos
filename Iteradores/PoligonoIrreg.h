1d2a9c422e78a6600183a35338e806712a3d461e
erictoman <ericto117@gmail.com>
2019-08-27 08:46:06 -0500

    Iteradores

---
+++ Iteradores/PoligonoIrreg.h
@@ -0,0 +1,17 @@
+#ifndef POLIGONOIRREG_H_
+#define POLIGONOIRREG_H_
+#include <vector>
+using namespace std;
+
+class PoligonoIrreg
+{
+private:
+	vector<Coordenada> vertices;
+
+public:
+	void imprimeVertices();
+	void anadeVertice(Coordenada coord);
+	void ordenaA();
+	PoligonoIrreg();
+};
+#endif
