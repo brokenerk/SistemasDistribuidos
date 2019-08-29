d5f70ea644cddb71b975adc9be19249e9c4bdc8c
brokenerk <enriquebroly@hotmail.com>
2019-08-29 11:32:22 -0500

    eje1

--- 7Strings/E1.cpp
+++ 7Strings/E1.cpp
@@ -1,8 +1,21 @@
 #include <string>
+#include <iostream>
+using namespace std;
 
-int main(){
+int main() {
 	int n;
 	string line;
+
 	cin >> n;
+	//cin.ignore();
 	getline(cin, line);
+
+	//getline(cin, line);
+	//cin >> n;
+	
+	cout << "String: " << line << endl;
+	cout << "Int: " << n << endl;
+	return 0;
 }
+
+//Link: https://es.stackoverflow.com/questions/20097/ayuda-con-funci%C3%B3n-cin-getline-c
