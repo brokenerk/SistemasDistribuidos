8415cd8f013353839df3b17da13ede587d831b72
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2019-09-01 17:00:30 -0500

    conflicts fixed

--- 1PrincipiosC++/E1.cpp
+++ 1PrincipiosC++/E1.cpp
@@ -2,21 +2,13 @@
 using namespace std;
 const double PI = 3.1415926;
 
-<<<<<<< HEAD
-int main()
-{
-=======
 int main() {
->>>>>>> 0bdaf2eb25f0f0b27d76870f719848c77da0e7aa
 	float radio;
 	cout << "Ingrese el radio del circulo: ";
 	cin >> radio;
-<<<<<<< HEAD
-	cout << PI * radio * radio << endl;
-	PI = 2;
-	cout << PI * radio * radio << endl;
-=======
+
 	cout << "Area: " << PI * radio * radio << endl;
->>>>>>> 0bdaf2eb25f0f0b27d76870f719848c77da0e7aa
+	//PI = 2;
+	cout << "Area: " << PI * radio * radio << endl;
 	return 0;
 }
--- 1PrincipiosC++/E3.cpp
+++ 1PrincipiosC++/E3.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int main() {
     double c = 20;
     double f;
-<<<<<<< HEAD
-    f = (9.0 / 5) * c + 32;
-    cout << f << endl;
-=======
+
     f = (9 / 5) * c + 32;
 	cout << "Farenheit: " << f << endl;
+
+    f = (9.0 / 5.0) * c + 32.0;
+    cout << "Farenheit: " << f << endl;;
 	return 0;
->>>>>>> 0bdaf2eb25f0f0b27d76870f719848c77da0e7aa
 }
