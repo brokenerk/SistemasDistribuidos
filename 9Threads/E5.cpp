b19f3dea2e1afe585ff9128e85bb448d4cbe1d2a
erictoman <ericto117@gmail.com>
2019-09-03 09:40:27 -0500

    CATMAN FULL

--- 9Threads/E5.cpp
+++ 9Threads/E5.cpp
@@ -1,22 +1,21 @@
-//g++ -std=c++11  E2.cpp -o E2 -lpthread
+//g++ -std=c++11  E5.cpp -o E5 -lpthread
 #include <iostream>
 #include <unistd.h>
 #include <thread>
 #include <atomic>
 using namespace std;
 int valor = 0;
-atomic<int> globalv(0);
-void decre()
+void decre(atomic<int> &globalv)
 {
     for (int i = 0; i < 10; i++)
     {
         globalv -= 1;
-        cout << "Hilo: " << this_thread::get_id() << " ejecutando inc() el valor es= " << globalv << endl;
+        cout << "Hilo: " << this_thread::get_id() << " ejecutando dec() el valor es= " << globalv << endl;
         sleep(1);
     }
 }
 
-void incre()
+void incre(atomic<int> &globalv)
 {
     for (int i = 0; i < 10; i++)
     {
@@ -28,11 +27,12 @@ void incre()
 
 int main()
 {
-    thread th1(incre), th2(decre);
+    atomic<int> globalv(0);
+    thread th1(incre, ref(globalv)), th2(decre, ref(globalv));
     printf("Proceso principal espera que los hilos terminen\n");
     th1.join();
     th2.join();
     printf("El hilo principal termina\n");
-    cout << "el valor es" << globalv << endl;
+    cout << "el valor es:" << globalv << endl;
     exit(0);
 }
