66c03db8847aa57259223800b8c14ce6e628c326
Marcus <rafassassinscreed@gmail.com>
2019-09-05 11:13:49 -0500

    el ejercicio 2

---
+++ 10Conveniencia/E2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+#define numeroElementos 10000000000
+int main(){
+ int *arreglo, i;
+ arreglo = new int[numeroElementos];
+ for(i = 0; i < numeroElementos; i++)
+ arreglo[i] = 0;
+ for(i = 0; i < 100000000; i++){
+ arreglo[rand()%numeroElementos] = rand();
+ }
+}
