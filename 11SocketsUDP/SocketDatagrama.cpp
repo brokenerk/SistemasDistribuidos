230120e13d0b07c3cec26fe9aa1720f2683bdafc
brokenerk <enriquebroly@hotmail.com>
2019-09-24 09:44:06 -0500

    intento clase socket

--- 11SocketsUDP/SocketDatagrama.cpp
+++ 11SocketsUDP/SocketDatagrama.cpp
@@ -19,7 +19,21 @@ SocketDatagrama::~SocketDatagrama() {
 }
 
 SocketDatagrama::recibe(PaqueteDatagrama & p) {
-	
+	/* rellena la dirección del servidor */
+   	bzero((char *)&direccionLocal, sizeof(direccionLocal));
+   	direccionLocal.sin_family = AF_INET;
+   	direccionLocal.sin_addr.s_addr = inet_addr(p.obtieneDireccion());
+   	bind(s, (struct sockaddr *)&direccionLocal, sizeof(direccionLocal));
+	int i = recvfrom(s, (char *)p.obtieneDatos(), p.obtieneLLongitud(), 0, NULL, NULL);
+
+	unsigned char ip[4];
+	memcpy(ip, &direccionForaneo.sin_addr.s_addr, 4);
+
+	printf("Mensaje recibido de:\n");
+	printf("IP: %d.%d.%d.%d \n", ip[0], ip[1], ip[2], ip[3]);
+	printf("Puerto: %d\n", direccionForaneo.sin_port);
+	printf("sizeof = %lu\n", p.obtieneLongitud());
+
 }
 
 SocketDatagrama::envia(PaqueteDatagrama & p) {
@@ -28,5 +42,6 @@ SocketDatagrama::envia(PaqueteDatagrama & p) {
    	direccionForaneo.sin_family = AF_INET;
    	direccionForaneo.sin_addr.s_addr = inet_addr(p.obtieneDireccion());
    	direccionForaneo.sin_port = htons(p.obtienePuerto());
-	return sendto(s, (char *)p.obtieneDatos(), p.obtieneLonguitud(), 0, (struct sockaddr *) &direccionForaneo, sizeof(direccionForaneo));
+   	bind(s, (struct sockaddr *)&direccionForaneo, sizeof(direccionForaneo));
+	return sendto(s, (char *)p.obtieneDatos(), p.obtieneLongitud(), 0, (struct sockaddr *) &direccionForaneo, sizeof(direccionForaneo));
 }
