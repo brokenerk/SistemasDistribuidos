d6bfb0351d8209394a8c636163c0d03f01c0dbc2
erictoman <ericto117@gmail.com>
2019-10-07 11:11:44 -0500

    13 DONE

--- 13Timeout/Solicitud.cpp
+++ 13Timeout/Solicitud.cpp
@@ -3,30 +3,42 @@
 #include <cstring>
 using namespace std;
 
-Solicitud::Solicitud() {
+Solicitud::Solicitud()
+{
 	socketlocal = new SocketDatagrama(0);
 }
 
-char* Solicitud::doOperation(char *IP, int puerto, int operationId, char *arguments) {
+char *Solicitud::doOperation(char *IP, int puerto, int operationId, char *arguments)
+{
 	struct mensaje sms;
 	sms.messageType = 0;
 	sms.requestId = 0;
 	sms.operationId = operationId;
-    memcpy(sms.arguments, arguments, sizeof(arguments));
-    
-	PaqueteDatagrama p = PaqueteDatagrama((char*)&sms, sizeof(sms), IP, puerto);
+	memcpy(sms.arguments, arguments, sizeof(arguments));
+	PaqueteDatagrama p = PaqueteDatagrama((char *)&sms, sizeof(sms), IP, puerto);
 	cout << "Direccion: " << p.obtieneDireccion() << endl;
 	cout << "Puerto: " << p.obtienePuerto() << endl;
 	socketlocal->envia(p);
-
 	PaqueteDatagrama p1 = PaqueteDatagrama(4000);
-    int tam = socketlocal->recibe(p1);
-    if (tam == -1) {
-      perror("Recvfrom failed");
-    }
-    cout << "\nMensaje recibido" << endl;
-    cout << "Direccion: " << p1.obtieneDireccion() << endl;
-    cout << "Puerto: " << p1.obtienePuerto() << endl;
-	struct mensaje* msj = (struct mensaje *)p1.obtieneDatos();
-    return (char *) msj->arguments;
+	int tam = socketlocal->recibeTimeout(p1, 2, 500);
+	int n = 1;
+	while (tam == -1 && n < 7)
+	{
+		socketlocal->envia(p);
+		tam = socketlocal->recibeTimeout(p1, 2, 500);
+		n++;
+	}
+	if (tam == -1 && n >= 7)
+	{
+		cout << "Servidor no esta disponible, intente mas tarde." << endl;
+		exit(0);
+	}
+	else
+	{
+		cout << "\nMensaje recibido" << endl;
+		cout << "Direccion: " << p1.obtieneDireccion() << endl;
+		cout << "Puerto: " << p1.obtienePuerto() << endl;
+		struct mensaje *msj = (struct mensaje *)p1.obtieneDatos();
+		return (char *)msj->arguments;
+	}
 }
