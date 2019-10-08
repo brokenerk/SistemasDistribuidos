36a7b53183bf0f8e734183f3beaf22ab0a4e0fc1
brokenerk <enriquebroly@hotmail.com>
2019-10-08 09:58:05 -0500

    pruebas que no mueren

--- 14Confiable/cliente.cpp
+++ 14Confiable/cliente.cpp
@@ -7,26 +7,29 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Forma de uso: %s ip_servidor num1 num2 n\n", argv[0]);
+		printf("Forma de uso: %s ip_servidor n\n", argv[0]);
 		exit(0);
 	}
 	int n = atoi(argv[2]);
 	int cont = 0;
-	int res = 0;
+	int res[64999];
 	int cuenta = 0;
 	while (cont < n)
 	{
+
 		int arr[64999];
 		Solicitud s;
 		arr[0] = randNum();
-		memcpy(&res, s.doOperation(argv[1], 7200, 1, (char *)&arr), 4);
+		memcpy(&res, s.doOperation(argv[1], 7200, 1, (char *)&arr), 64999);
 		cuenta = cuenta + arr[0];
-		if (cuenta != res)
+		if (cuenta != res[0])
 		{
-			printf("Respuesta: %d \nOriginal: %d\n\n", cuenta, res);
+			cout << "entro al if" << endl;
+			printf("Num: %d\nRespuesta: %d \nOriginal: %d\n\n", cont, cuenta, res[0]);
 			exit(0);
 		}
 		cont++;
+		printf("Num: %d\nRespuesta: %d \nOriginal: %d\n\n", cont, cuenta, res[0]);
 	}
 	return 0;
 }
