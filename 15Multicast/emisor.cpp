531f4149359dbe8b6ef330706f42d67dcc3f202e
brokenerk <enriquebroly@hotmail.com>
2019-10-29 08:36:26 -0600

    emisor falta suma

--- 15Multicast/emisor.cpp
+++ 15Multicast/emisor.cpp
@@ -1,13 +1,19 @@
 #include "SocketMulticast.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+	if(argc != 3) {
+        printf("Forma de uso: %s ip_servidor puerto \n", argv[0]);
+		exit(0);
+    }
+
     SocketMulticast sm = SocketMulticast(0);
     PaqueteDatagrama p = PaqueteDatagrama(100);
     p.inicializaIp(argv[1]);
     p.inicializaPuerto(atoi(argv[2]));
     p.inicializaDatos("Hola mundo");
-    while((sm.envia(p,10))>0){
-        cout << "Mensaje enviado" << endl;
+
+    while((sm.envia(p,10)) > 0) {
+        cout << "\nMensaje enviado" << endl;
         cout << "IP: " << p.obtieneDireccion() << endl;
         cout << "Puerto: " << p.obtienePuerto() << endl;
         cout << "Datos: " << p.obtieneDatos() << endl; 
--- 15Multicast/receptor.cpp
+++ 15Multicast/receptor.cpp
@@ -1,14 +1,17 @@
 #include "SocketMulticast.h"
-int main(int argc, char *argv[]){
-    if(argc != 3){
+
+int main(int argc, char *argv[]) {
+    if(argc != 3) {
         printf("Forma de uso: %s ip_servidor puerto \n", argv[0]);
 		exit(0);
     }
+
     SocketMulticast sm = SocketMulticast(atoi(argv[2]));
     PaqueteDatagrama p = PaqueteDatagrama(100);
     sm.unirseGrupo(argv[1]);
-    while(sm.recibe(p)){
-        cout << "Mensaje recibido" << endl;
+
+    while(sm.recibe(p)) {
+        cout << "\nMensaje recibido" << endl;
         cout << "IP: " << p.obtieneDireccion() << endl;
         cout << "Puerto: " << p.obtienePuerto() << endl;
         cout << "Datos: " << p.obtieneDatos() << endl; 
