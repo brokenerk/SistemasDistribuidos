300a5aceb51331abe97dec18046595e19ae42dc8
brokenerk <enriquebroly@hotmail.com>
2019-11-04 10:41:46 -0600

    pdf multicast conf

---
+++ 16MulticastConfiable/receptor.cpp
@@ -0,0 +1,40 @@
+#include "SocketMulticast.h"
+#include "SocketDatagrama.h"
+
+
+int main(int argc, char *argv[]) {
+    if(argc != 3) {
+        printf("Forma de uso: %s ip_servidor puerto \n", argv[0]);
+		exit(0);
+    }
+
+    SocketMulticast sm = SocketMulticast(atoi(argv[2]));
+    PaqueteDatagrama p = PaqueteDatagrama(100);
+    sm.unirseGrupo(argv[1]);
+
+    while(sm.recibe(p)) {
+        cout << "\nMensaje recibido" << endl;
+        cout << "IP: " << p.obtieneDireccion() << endl;
+        cout << "Puerto: " << p.obtienePuerto() << endl;
+        //cout << "Datos: " << p.obtieneDatos() << endl; 
+        int n[2];
+        memcpy(&n , p.obtieneDatos(), 8);
+        int resul[1];
+        resul[0]= n[0]+n[1]; 
+       
+        cout <<"numero 1: " << n[0] << endl;
+        cout <<"numero 2: " << n[1] << endl;
+        cout << "Longitud: " << p.obtieneLongitud() << endl;
+        cout << "re: " << resul[0] << endl;
+        PaqueteDatagrama respuesta = PaqueteDatagrama(4);
+        respuesta.inicializaIp(p.obtieneDireccion());
+        respuesta.inicializaDatos((char * )resul);
+        respuesta.inicializaPuerto(7200);
+        SocketDatagrama  sr = SocketDatagrama(0);
+        sr.envia(respuesta);
+
+
+
+    }
+    return 0;
+}
